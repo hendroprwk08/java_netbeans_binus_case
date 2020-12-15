3597b37b649943c23794791cf45c8501089b1031
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2020-12-15 09:44:37 +0700

    Initial commit

---
+++ src/binuscase/Payment.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binuscase;
+
+/**
+ *
+ * @author hendr
+ */
+public class Payment {
+    String name, type, sugar;
+    int qty;
+
+    public Payment(String name, String type, String sugar, int qty ) {
+        this.name = name;
+        this.type = type;
+        this.sugar = sugar;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public String getSugar() {
+        return sugar;
+    }
+}
